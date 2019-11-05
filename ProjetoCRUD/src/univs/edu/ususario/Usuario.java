daeb4113da757373c7cb2fe8fabf97759d8aa5ca
elliard <eliardepatricio@gmail.com>
2019-11-05 20:56:34 -0200

    CRUD Inicial

---
+++ ProjetoCRUD/src/univs/edu/ususario/Usuario.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package univs.edu.ususario;
+
+/**
+ *
+ * @author LABORATORIO 01
+ */
+public class Usuario {
+    private int idUsuario;
+    
+    private String nomeUsuario;
+    
+    private String login;
+    
+    private String senha;
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getNomeUsuario() {
+        return nomeUsuario;
+    }
+
+    public void setNomeUsuario(String nomeUsuario) {
+        this.nomeUsuario = nomeUsuario;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
