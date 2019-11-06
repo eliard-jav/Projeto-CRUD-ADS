ea2442af269189ca93176468d91ddf8249402332
eliard <eliardepatricio@gmail.com>
2019-11-06 20:58:28 -0200

    Modelo Inicial

--- ProjetoCRUD/src/univs/edu/ususario/Usuario.java
+++ ProjetoCRUD/src/univs/edu/ususario/Usuario.java
@@ -5,17 +5,23 @@
  */
 package univs.edu.ususario;
 
-/**
- *
- * @author LABORATORIO 01
- */
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+
 public class Usuario {
+    @Id
+    @GeneratedValue
     private int idUsuario;
     
+    @Column(length = 100, nullable = false)
     private String nomeUsuario;
-    
+    @Column(length = 100, nullable = false, unique = true)
     private String login;
-    
+    @Column(length = 100, nullable = false)
     private String senha;
 
     public int getIdUsuario() {
